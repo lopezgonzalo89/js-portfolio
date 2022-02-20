24138198a93e6739dcb1523d96b8f1fc66ae5a7e
Lopez Gonzalo <lopezgonzalo.sm89@gmail.com>
2022-02-20 18:55:22 -0300

    ready to deploy

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -1,4 +1,5 @@
-import getData from '../utils/getData.js';
+import getData from "@utils/getData.js";
+import instagram from "@images/instagram.png";
 
 const Template = async () => {
   const data = await getData();
@@ -24,13 +25,13 @@ const Template = async () => {
         </div>
         <div class="card_social">
           <a href="https://twitter.com/gndx">
-            <img src="../src/assets/images/twitter.png" />
+            <img src="assets/images/twitter.png" />
           </a>
           <a href="https://github.com/gndx">
-            <img src="../src/assets/images/github.png" />
+            <img src="assets/images/github.png" />
           </a>
           <a href="https://instagram.com/gndx">
-            <img src="../src/assets/images/instagram.png" />
+            <img src="${instagram}" />
           </a>
         </div>
       </div>
---
+++ webpack.config.dev.js
@@ -0,0 +1,77 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebPackPlugin = require("copy-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[main].[contenthash].js",
+    assetModuleFilename: "assets/images/[hash][ext][query]",
+  },
+  mode: "development",
+  watch: true,
+  resolve: {
+    extensions: [".js"],
+    alias: {
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@templates": path.resolve(__dirname, "src/templates"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@images": path.resolve(__dirname, "src/assets/images"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.png/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff",
+            name: "[name].[contenthash].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "../assets/fonts/",
+            esModule: false,
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebPackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images",
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "assets/[name].[contenthash].css",
+    }),
+    new Dotenv(),
+  ],
+};
